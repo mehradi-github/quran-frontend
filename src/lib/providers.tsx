5a2a2bb535737c8339f937c9f2feb167f2b4272c
Mehradi <mehradi.info@gmail.com>
2023-07-10 10:11:33 +0200

    Adding ApolloProvider

--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -1,11 +1,17 @@
-'use client'
+'use client';
 
 /* Core */
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
 
+import { ApolloProvider } from '@apollo/client';
 /* Instruments */
-import { reduxStore } from '@/lib/redux'
+import { reduxStore } from '@/lib/redux';
+import { apolloClient } from '@/graphql';
 
 export const Providers = (props: React.PropsWithChildren) => {
-  return <Provider store={reduxStore}>{props.children}</Provider>
-}
+  return (
+    <Provider store={reduxStore}>
+      <ApolloProvider client={apolloClient}>{props.children}</ApolloProvider>
+    </Provider>
+  );
+};
